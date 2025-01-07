a2fab5529722a07d06dedcab0d2537ae0e7d4a27
rdadkins01 <rdadkins01@yahoo.com>
2025-01-07 12:58:18 -0500

    Added asteroids, circles, player, updasted contstants and game loop logic.

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
+import sys
 import pygame
 from constants import *
+from player import *
+from shot import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.conainers = (shots, updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+
+        for item in drawable:
+            item.draw(screen)
+
+        for item in updatable:
+            item.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+
         pygame.display.flip()
+
+        # Limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
